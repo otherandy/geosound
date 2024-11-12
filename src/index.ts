d398578c108622d1367256ad1a8c5472989badce
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-11-11 21:13:19 -0800

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import fileUpload from "express-fileupload";
+
 import audioRouter from "./routes/audio";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024, files: 1 },
