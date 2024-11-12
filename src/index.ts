e8b401bde7c50f33911c7b36e9964e7f7646fa58
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-11-11 20:27:02 -0800

    added middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(new Date(), req.method, req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send(new Date());
 });
